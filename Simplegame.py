c6b1020dcf04a7efb67666054afab3d2f903d629
Pranavchanagond <112197866+Pranavchanagond@users.noreply.github.com>
2022-11-22 14:39:38 +0530

    Update Simplegame.py

--- Simplegame.py
+++ Simplegame.py
@@ -2,16 +2,30 @@ import random
 import time
 import os
 
-letter=["A","B","C","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+# letter=["A","B","C","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+letter=["absolutely","accepted","acclaimed","accomplish" ," accomplishment" ," achievement" ," action" ," active" ," admire" ," adorable" ," adventure" ," affirm " ,
+    "ative " ,"affluent " ,"agree" ," agree " ,"ableam " ,"azing" ,"angelic " ,"appealing " ,"approve " ,"aptitude " ,"attractive" ,"awesome",
+    "beaming","beautiful","believe","beneficial","bliss","bountiful","bounty","brave","bravo","brilliant","bubbly",
+    "calm","celebrated","certain","champ","champion","charming","cheery","choice","classic","classical","clean","commend","composed","congratulation","constant","cool",
+    "courageous","creative","cute"]
 str2=""
+
+def ran(letter):
+    str1=((str)(random.choices(letter)))
+    return str1
 
 for result in range(0,10):
     if(result < 4):
-        str2 = ((str)(random.choices(letter,k=3)))
+        print(f"{'LEVEL-1':^50}")
+        str2 = ran(letter)
     elif(result < 7):
-        str2 = ((str)(random.choices(letter,k=6)))
+        
+        print(f"{'LEVEL-2':^50}")
+        str2 = ran(letter)
     else:
-        str2 = ((str)(random.choices(letter,k=8)))
+        print(f"{'LEVEL-3':^50}")
+        str2 = ran(letter)
+    str2=str2.upper()
     retype=""
     for i in str2:
         if (ord(i) >= 65 and ord(i) <= 90):
@@ -19,7 +33,7 @@ for result in range(0,10):
     
     for i in range(0,1):
         print(retype),
-    time.sleep(4)    
+    time.sleep(2)    
     os.system("cls")
     Player=input("Enter above letters as it is : ")
     os.system("cls")
